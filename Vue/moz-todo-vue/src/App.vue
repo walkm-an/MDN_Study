3ac3fd8ac3ae053b74bd2723079075454a7450b5
walkm-an <z5110583@ad.unsw.edu.au>
2021-01-29 15:19:59 +1100

    TODO vue component v1 finished

--- Vue/moz-todo-vue/src/App.vue
+++ Vue/moz-todo-vue/src/App.vue
@@ -1,26 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="app">
+        <h1>
+            To-Do List
+        </h1>
+        <ul>
+            <li v-for="item in ToDoItems" v-bind:key="item.id">
+                <to-do-item :label="item.label" :done="item.done" :id="item.id"></to-do-item>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import ToDoItem from './components/ToDoItem.vue';
+import uniqueID from 'lodash.uniqueid';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "app",
+    components:{
+        ToDoItem
+    },
+    data() {
+        return {
+            ToDoItems : [
+                {id:uniqueID('todo-'), label: 'learn vue', done: false},
+                {id:uniqueID('todo-'), label: 'create vue', done: true},
+                {id:uniqueID('todo-'), label: 'have vue', done: true},
+                {id:uniqueID('todo-'), label: 'learn blablabla', done: false}
+            ]
+        };
+    }
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
